70443ced18e9d2be41de185fa93001401f22ff49
Faruk Yildirim <fyildirim21@ku.edu.tr>
2026-03-04 11:37:33 +0300

    chore: initial Flutter project setup

---
+++ android/app/src/main/kotlin/com/hubx/plant_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hubx.plant_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,6 @@
+// ignore: unused_import
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // Widget tests will be added per feature
+}
